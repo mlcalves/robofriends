3617c8b978f9e346ec1094680cd542e0898cd27c
Mario Alves <mlcalves@gmail.com>
2023-05-17 06:23:01 +0100

    projeto final

---
+++ src/components/CardList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+    //em vez de map() pode usar-se o foreach()
+    //Loop
+    return (
+        <div>
+            {
+                robots.map((user, i) => {
+
+                    // {cardComponent} e {i}  têm de estar entre {} derivado de ser javascript
+                    return (
+                        <Card
+                            key={i}
+                            id={robots[i].id}
+                            name={robots[i].name}
+                            email={robots[i].email}
+                        />
+                    );
+                })
+            }
+        </div>
+    );
+}
+
+export default CardList;
+
